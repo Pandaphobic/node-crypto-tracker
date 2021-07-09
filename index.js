a2b775ac3105419c3c27daaa51f0d2852cbe4c26
Chris Steffes <c.steffes@live.ca>
2021-07-09 13:41:51 -0500

    minor fix at getAllCoins + functionalization

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const fs = require("fs")
 const toml = require("toml")
 require("dotenv").config()
 
-const config = toml.parse(fs.readFileSync("./config.toml", "utf-8"))
+let allCoinsList = ""
+let coinPrices = ""
+let gasPrice = ""
 
-// ** Everything below can be referenced globally and is updated at app start **
-// -- GLOBAL API DATA -- //
-var allCoinsList = async () => {
+// Config + Imported From Config
+const config = toml.parse(fs.readFileSync("./config.toml", "utf-8"))
+const WIDTH = 32
+const COINS_TO_GET = config.ticker.coins.join("%2C")
+const VS_CURRENCY = config.ticker.vsCurrency.toLowerCase()
+const REFRESH_RATE = config.ticker.refreshRate // in Seconds
+
+async function updateGasPrice() {
   try {
-    const res = await fetch("https://api.coingecko.com/api/v3/coins/list?include_platform=true")
+    const res = await fetch(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.ETH_GAS_API_KEY}`)
     const data = await res.json()
     return data
   } catch (err) {
@@ -18,47 +25,30 @@ var allCoinsList = async () => {
     return err
   }
 }
-// let allCoinsList = `` // id, symbol etc
-let coinPrices = `` // price in vs_currency for each coin in config
-let gasPrice = ``
+
+async function updateCoinPrices() {
+  // FINAL URL for FETCH
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${COINS_TO_GET}&vs_currencies=${VS_CURRENCY}&include_24hr_change=true&include_last_updated_at=true`
+
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
+}
+
+async function getAllCoins() {
+  try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/list?include_platform=true")
+    const data = await res.json()
+    allCoinsList = data
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 // Import Modules
 const { aaveHealthFactor } = require("./modules/aavehealthfactor.js")
 const { ticker } = require("./modules/ticker")
-
-// Config + Imported From Config
-const WIDTH = 32
-const COINS_TO_GET = config.ticker.coins.join("%2C")
-
-const VS_CURRENCY = config.ticker.vsCurrency.toLowerCase()
-const REFRESH_RATE = config.ticker.refreshRate // in Seconds
-
-// API CALL FOR COINS + PRICES + 24HRCHANGE
-const updateCoinPrices = async () => {
-  // FINAL URL for FETCH
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${COINS_TO_GET}&vs_currencies=${VS_CURRENCY}&include_24hr_change=true&include_last_updated_at=true`
-
-  try {
-    const res = await fetch(url)
-    coinPrices = await res.json()
-    return coinPrices
-  } catch (err) {
-    console.log(err) //can be console.error
-    return err
-  }
-}
-// API CALL FOR GASPRICE
-const updateGasPrice = async () => {
-  try {
-    const res = await fetch(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.ETH_GAS_API_KEY}`)
-    const data = await res.json()
-
-    return data
-  } catch (err) {
-    console.log(err)
-    return err
-  }
-}
 
 const separator = char => {
   const arr = new Array(WIDTH).fill(char)
@@ -66,11 +56,13 @@ const separator = char => {
 }
 
 // Main API Call
-const main = (async () => {
+async function main() {
   // API CALLS
-  allCoinsList = await allCoinsList()
-  coinPrices = await updateCoinPrices()
-  gasPrice = await updateGasPrice()
+  if (!allCoinsList) {
+    allCoinsList = await getAllCoins() // id, symbol etc
+  }
+  coinPrices = await updateCoinPrices() // price in vs_currency for each coin in config
+  gasPrice = await updateGasPrice() // get avgerage gas price in gwei
 
   // START OF DRAW
   console.clear()
@@ -83,7 +75,9 @@ const main = (async () => {
   separator("-")
   console.log(chalk.bgGrey.bold(`🔥 Gas Price Avg.           ${`${chalk.whiteBright(gasPrice.average)}`} `))
   aaveHealthFactor(coinPrices, VS_CURRENCY, config)
-})()
+}
+// Init
+main()
 
 // SET HOW MANY SECONDS BETWEEN REFRESHES
 var seconds = REFRESH_RATE
--- modules/ticker.js
+++ modules/ticker.js
@@ -14,28 +14,26 @@ function ticker(data, allCoinsList, VS_CURRENCY) {
     console.log(chalk.bold.bgBlueBright(`         Crypto Ticker       USD`))
     const rows = []
 
-    if (rows) {
-      for (coin in data) {
-        // Build the Row
-        let symbol = findCoinById(coin, allCoinsList).symbol.toUpperCase()
-        let price = `${data[coin][VS_CURRENCY]}`
-        let change24hr = `${data[coin][`${VS_CURRENCY}_24h_change`].toFixed(2)}%`
+    for (coin in data) {
+      // Build the Row
+      let symbol = findCoinById(coin, allCoinsList).symbol.toUpperCase()
+      let price = `${data[coin][VS_CURRENCY]}`
+      let change24hr = `${data[coin][`${VS_CURRENCY}_24h_change`].toFixed(2)}%`
 
-        const row = [symbol, price, change24hr, symbol.length, price.length, change24hr.length]
-        const neg = row[2].includes("-") ? true : false
+      const row = [symbol, price, change24hr]
+      const neg = row[2].includes("-") ? true : false
 
-        // Add Spacing
-        const a = new Array(13 - row[0].length).fill(" ")
-        const space1 = a.join("")
-        const b = new Array(neg ? 13 - row[1].length : 14 - row[1].length).fill(" ")
-        const space2 = b.join("")
+      // Add Spacing
+      const a = new Array(13 - row[0].length).fill(" ")
+      const space1 = a.join("")
+      const b = new Array(neg ? 13 - row[1].length : 14 - row[1].length).fill(" ")
+      const space2 = b.join("")
 
-        const processedRow = `${chalk.cyanBright(row[0])}${space1}${chalk.yellow(row[1])}${space2}${neg ? chalk.redBright(row[2]) : chalk.greenBright(row[2])}`
-        rows.push(processedRow)
-      }
-      for (i = 0; i < rows.length; i++) {
-        console.log(rows[i])
-      }
+      const processedRow = `${chalk.cyanBright(row[0])}${space1}${chalk.yellow(row[1])}${space2}${neg ? chalk.redBright(row[2]) : chalk.greenBright(row[2])}`
+      rows.push(processedRow)
+    }
+    for (i = 0; i < rows.length; i++) {
+      console.log(rows[i])
     }
   } catch (err) {
     console.log(`ticker error: ${err}`)
