ee65657b0da6002f8d5be76eb849910c1a22d377
Chris Steffes <c.steffes@live.ca>
2021-07-16 08:08:29 -0500

    weth-usd bug fix

--- index.js
+++ index.js
@@ -1,93 +1,83 @@
-var fetch = require("node-fetch");
-var chalk = require("chalk");
-const fs = require("fs");
-const TOML = require("@iarna/toml");
-require("dotenv").config();
+var fetch = require("node-fetch")
+var chalk = require("chalk")
+const fs = require("fs")
+const TOML = require("@iarna/toml")
+require("dotenv").config()
 
 // Config + Imported From Config
-const config = TOML.parse(fs.readFileSync("./config.toml", "utf-8"));
-const WIDTH = 32;
-const COINS_TO_GET = config.ticker.coins.join("%2C");
-const VS_CURRENCY = config.ticker.vsCurrency.toLowerCase();
-const REFRESH_RATE = config.ticker.refreshRate; // in Seconds
+const config = TOML.parse(fs.readFileSync("./config.toml", "utf-8"))
+const WIDTH = 32
+const COINS_TO_GET = config.ticker.coins.join("%2C")
+const VS_CURRENCY = config.ticker.vsCurrency.toLowerCase()
+const REFRESH_RATE = config.ticker.refreshRate // in Seconds
 
 // ***** IMPORT MODULES *****
-const { aaveHealthFactor } = require("./modules/aavehealthfactor.js");
-const { ticker } = require("./modules/ticker");
-const { printGasPrice } = require("./modules/gasPrices");
+const { aaveHealthFactor } = require("./modules/aavehealthfactor.js")
+const { ticker } = require("./modules/ticker")
+const { printGasPrice } = require("./modules/gasPrices")
 
 // Separator Module
 const separator = char => {
-  const arr = new Array(WIDTH).fill(char);
-  console.log(arr.join(""));
-};
+  const arr = new Array(WIDTH).fill(char)
+  console.log(arr.join(""))
+}
 
 // ***** API CALLS  *****
 const getAllCoinsList = async () => {
   try {
-    const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
-    );
-    const coinsList = await res.json();
-    return coinsList;
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/list?include_platform=true")
+    const coinsList = await res.json()
+    return coinsList
   } catch (err) {
-    console.log(`LIST CALL FAILED: 03 ${err}`);
+    console.log(`LIST CALL FAILED: 03 ${err}`)
   }
-};
+}
 
 const getCoinPrices = async () => {
   try {
-    const res = await fetch(
-      `https://api.coingecko.com/api/v3/simple/price?ids=${COINS_TO_GET}&vs_currencies=${VS_CURRENCY}&include_24hr_change=true&include_last_updated_at=true`
-    );
-    const coinPrices = await res.json();
-    return coinPrices;
+    const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${COINS_TO_GET}&vs_currencies=${VS_CURRENCY}&include_24hr_change=true&include_last_updated_at=true`)
+    const coinPrices = await res.json()
+    return coinPrices
   } catch (err) {
-    console.log(`PRICE CALL FAILED: 02 ${err}`);
+    console.log(`PRICE CALL FAILED: 02 ${err}`)
   }
-};
+}
 
 const getGasPrice = async () => {
   try {
-    const res = await fetch(
-      `https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.ETH_GAS_API_KEY}`
-    );
-    const gasPrices = await res.json();
-    return gasPrices;
+    const res = await fetch(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.ETH_GAS_API_KEY}`)
+    const gasPrices = await res.json()
+    return gasPrices
   } catch (err) {
-    console.log(`GAS CALL FAILED: 01 ${err}`);
+    console.log(`GAS CALL FAILED: 01 ${err}`)
   }
-};
+}
 
 async function main() {
   // UPDATE DATA
   if (!allCoinsList) {
-    var allCoinsList = await getAllCoinsList(); // IDs, names, details of coins
+    var allCoinsList = await getAllCoinsList() // IDs, names, details of coins
   }
-  var coinPrices = await getCoinPrices(); // price in vs_currency for each coin in config
-  var gasPrices = await getGasPrice(); // get avgerage gas price in gwei
+  var coinPrices = await getCoinPrices() // price in vs_currency for each coin in config
+  var gasPrices = await getGasPrice() // get avgerage gas price in gwei
   // START OF DRAW
-  console.clear();
-  separator("-");
-  console.log(
-    `🚀 ${chalk.bold.magentaBright("Welcome to Crypto Tracker!")} 🚀`
-  );
-  separator("-");
-  ticker(coinPrices, allCoinsList, VS_CURRENCY);
-  separator("-");
-  console.log(
-    `${chalk.bold(chalk.bgWhite.black(`      ✨ DeFi Dashboard ✨      `))}`
-  );
-  separator("-");
-  printGasPrice(gasPrices);
-  aaveHealthFactor(coinPrices, VS_CURRENCY);
+  console.clear()
+  separator("-")
+  console.log(`🚀 ${chalk.bold.magentaBright("Welcome to Crypto Tracker!")} 🚀`)
+  separator("-")
+  ticker(coinPrices, allCoinsList, VS_CURRENCY)
+  separator("-")
+  console.log(`${chalk.bold(chalk.bgWhite.black(`      ✨ DeFi Dashboard ✨      `))}`)
+  separator("-")
+  printGasPrice(gasPrices)
+  aaveHealthFactor()
 }
 // Init
-main();
+main()
 
 // SET HOW MANY SECONDS BETWEEN REFRESHES
-var seconds = REFRESH_RATE;
-var interval = seconds * 1000;
+var seconds = REFRESH_RATE
+var interval = seconds * 1000
 setInterval(function () {
-  main();
-}, interval);
+  main()
+}, interval)
--- modules/aavehealthfactor.js
+++ modules/aavehealthfactor.js
@@ -6,38 +6,34 @@
   More information: https://docs.aave.com/risk/asset-risk/risk-parameters#health-factor
 
 */
-var chalk = require("chalk");
-const fs = require("fs");
-const TOML = require("@iarna/toml");
-const path = require("path");
+var chalk = require("chalk")
+const fetch = require("node-fetch")
+const fs = require("fs")
+const TOML = require("@iarna/toml")
+const path = require("path")
 
-const config = TOML.parse(
-  fs.readFileSync(path.resolve(__dirname, "../config.toml"), "utf-8")
-);
+const config = TOML.parse(fs.readFileSync(path.resolve(__dirname, "../config.toml"), "utf-8"))
 
-async function aaveHealthFactor(coinPrices, VS_CURRENCY) {
+async function aaveHealthFactor() {
+  const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=weth&vs_currencies=usd")
+  const data = await res.json()
+
   // Externals
-  const wethPrice = await coinPrices.weth[VS_CURRENCY];
-  const currency = await VS_CURRENCY.toUpperCase();
+  const wethPriceUsd = await data.weth.usd
 
   // Collateral
-  const collateralInEth = config.aave.collateralInEth; // TO BE SET IN CONFIG
+  const collateralInEth = config.aave.collateralInEth // TO BE SET IN CONFIG
   // const collateralInCurrency = (collateralInEth * wethPrice).toFixed(2);
 
   // Borrowed
-  const borrowedInUSD = config.aave.borrowedInUsd;
-  const borrowedInEth = borrowedInUSD / wethPrice;
+  const borrowedInUSD = config.aave.borrowedInUsd
+  const borrowedInEth = borrowedInUSD / wethPriceUsd
   // const borrowedInCurrency = (borrowedInEth * wethPrice).toFixed(2);
 
   // Liquidation Threshold set by AAVE - Static
-  const LT = 0.825;
-  const healthFactor = ((collateralInEth * LT) / borrowedInEth).toFixed(2);
-
-  console.log(
-    `${chalk.bold.bgMagenta(`👻 AAVE Health Factor     ~${healthFactor} `)}`
-  );
-  // console.log(`  Collateral....${collateralInCurrency} ${currency}`);
-  // console.log(`  Borrowed.......${borrowedInCurrency} ${currency}`);
+  const LT = 0.825
+  const healthFactor = ((collateralInEth * LT) / borrowedInEth).toFixed(2)
+  console.log(`${chalk.bold.bgMagenta(`👻 AAVE Health Factor     ~${healthFactor} `)}`)
 }
 
-module.exports = { aaveHealthFactor };
+module.exports = { aaveHealthFactor }
